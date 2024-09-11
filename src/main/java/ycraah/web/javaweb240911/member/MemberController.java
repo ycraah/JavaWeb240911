279efdc9df3748a22a8b70461f3e7be0d64d12ed
ycraah <ycraah@gmail.com>
2024-09-11 22:00:38 +0900

    memberController ì작성

--- src/main/java/ycraah/web/javaweb240911/member/MemberController.java
+++ src/main/java/ycraah/web/javaweb240911/member/MemberController.java
@@ -1,4 +1,39 @@
 package ycraah.web.javaweb240911.member;
 
-public class MemberController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/mem.do")
+public class MemberController extends HttpServlet {
+  MemberDAO dao; //dao를 멤버 변수로 설정하여 서블릿이 살아있는 동안 다른 메서드에서 사용 가능하게 함
+  @Override
+  public void init() throws ServletException {
+    dao = new MemberDAO();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    doHandle(req,resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    doHandle(req,resp);
+  }
+
+  private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    req.setCharacterEncoding("UTF-8");
+    resp.setContentType("text/html;charset=UTF-8");
+    List<MemberVO> membersList = dao.listMembers();
+    req.setAttribute("membersList", membersList);
+    req.getRequestDispatcher("/WEB-INF/jsp/member.jsp").forward(req, resp);
+
+  }
+
 }
--- src/main/java/ycraah/web/javaweb240911/member/MemberDAO.java
+++ src/main/java/ycraah/web/javaweb240911/member/MemberDAO.java
@@ -1,4 +1,9 @@
 package ycraah.web.javaweb240911.member;
 
+import java.util.List;
+
 public class MemberDAO {
+  public List<MemberVO> listMembers() {
+
+  }
 }
